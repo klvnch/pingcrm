5e08c0ae2aca92ead52707d1ad4454a82bd6062a
Kelvin <kelvinchen270@gmail.com>
2022-02-22 16:57:14 +0700

    fix: fix soft delete bugs

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -11,6 +11,11 @@ class Customer extends Model
     use HasFactory;
     use SoftDeletes;
 
+    public function resolveRouteBinding($value, $field = null)
+    {
+        return $this->where($field ?? 'id', $value)->withTrashed()->firstOrFail();
+    }
+
     protected $dates = ['deleted_at'];
 
     public function contacts(){
